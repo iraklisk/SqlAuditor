397e0e7e9281f7ed1c936691164be72859ee140e
Iraklis Karagkiozoglou <karagkiozoglou.iraklis@ydrogios.gr>
2015-05-04 17:30:22 +0300

    Add untracked files

---
+++ SqlAuditor.Win/frmMain.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SqlAuditor.Win
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.metroStyleExtender1 = new MetroFramework.Components.MetroStyleExtender(this.components);
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.flInstances = new System.Windows.Forms.FlowLayoutPanel();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Controls.Add(this.metroTabPage3);
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 2;
+            this.metroTabControl1.Size = new System.Drawing.Size(675, 475);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(667, 433);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Home";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(20, 18);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(156, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Welcome to SQL Auditor";
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(667, 433);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Audit Viewer";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.Controls.Add(this.flInstances);
+            this.metroTabPage3.Controls.Add(this.metroLabel2);
+            this.metroTabPage3.Controls.Add(this.metroLabel3);
+            this.metroTabPage3.Controls.Add(this.metroTile1);
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.HorizontalScrollbarSize = 10;
+            this.metroTabPage3.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.Size = new System.Drawing.Size(667, 433);
+            this.metroTabPage3.TabIndex = 2;
+            this.metroTabPage3.Text = "Settings";
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            this.metroTabPage3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.VerticalScrollbarSize = 10;
+            // 
+            // metroStyleExtender1
+            // 
+            this.metroStyleExtender1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(94, 17);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(130, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "SQL Server Instances";
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(3, 58);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(85, 69);
+            this.metroTile1.TabIndex = 4;
+            this.metroTile1.Text = "Add";
+            this.metroTile1.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile1.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(0, 17);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(51, 19);
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "Actions";
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            // 
+            // flInstances
+            // 
+            this.flInstances.BackColor = System.Drawing.Color.Transparent;
+            this.flInstances.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flInstances.Location = new System.Drawing.Point(94, 55);
+            this.flInstances.Name = "flInstances";
+            this.flInstances.Size = new System.Drawing.Size(573, 313);
+            this.flInstances.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 555);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "frmMain";
+            this.Text = "SqlAuditor";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage3.ResumeLayout(false);
+            this.metroTabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Components.MetroStyleExtender metroStyleExtender1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private System.Windows.Forms.FlowLayoutPanel flInstances;
+    }
+}
+
---
+++ SqlAuditor.Win/frmMain.cs
@@ -0,0 +1,92 @@
+﻿using MetroFramework.Controls;
+using MetroFramework.Forms;
+using SqlAuditor.Config;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SqlAuditor.Win
+{
+    public partial class frmMain : MetroForm
+    {
+        private AuditConfig auditConfig;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadConfig()
+        {
+            if (!File.Exists("Config.xml"))
+            {
+                auditConfig = new AuditConfig();
+            }
+            else
+            {
+                auditConfig = AuditConfig.Load("Config.xml");
+            }
+        }
+
+        void ReloadTraceInstances()
+        {
+            flInstances.SuspendDrawing();
+            flInstances.Controls.Clear();
+            foreach (var trace in auditConfig.Traces)
+            {
+                var mtile = new MetroTile();
+
+                mtile.ActiveControl = null;
+                mtile.Location = new System.Drawing.Point(171, 3);
+                mtile.Size = new System.Drawing.Size(162, 69);
+                mtile.TabIndex = 4;
+                mtile.Text = trace.Instance.DataSource;
+                mtile.UseSelectable = true;
+                mtile.Tag = trace;
+                mtile.Click += mtile_Click;
+                this.flInstances.Controls.Add(mtile);
+            }
+            flInstances.ResumeDrawing();
+        }
+
+        void mtile_Click(object sender, EventArgs e)
+        {
+         ShowSettings(false,((sender as MetroTile).Tag as TraceConfig));  
+        }
+        private void SaveConfig()
+        {
+            auditConfig.Save("Config.xml");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadConfig();
+            ReloadTraceInstances();
+        }
+
+        private void ShowSettings(bool isnew, TraceConfig trace)
+        {
+            if (new frmSettings(isnew, trace).ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                auditConfig.Save("Config.xml");
+            }
+            else
+            {
+                LoadConfig();
+            }
+            ReloadTraceInstances();
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            ShowSettings(true, new TraceConfig(new InstanceConfig("localhost", "SqlAudit")));
+        }
+
+    }
+}
